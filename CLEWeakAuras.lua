5a583261c687e0d7e840836913236c7cf523c0a8
veepee <66334129+vpontual@users.noreply.github.com>
2024-10-30 13:52:55 -0700

    fixed bugs in creating WA displays

--- CLEWeakAuras.lua
+++ CLEWeakAuras.lua
@@ -36,19 +36,70 @@ local icons = {
     }
 }
 
--- Create a WeakAura module for each message type
-for msgType, data in pairs(addon.messageTypes) do
-    if icons[msgType] then
-        weakaura:RegisterModule(msgType, {
-            name = icons[msgType].name,
-            texture = icons[msgType].texture,
-            color = icons[msgType].color,
-            size = icons[msgType].size,
-            scale = icons[msgType].scale,
-            show = icons[msgType].show,
-            hide = icons[msgType].hide,
-        })
-    end
+-- Store WeakAuras display objects
+local displays = {}
+
+-- Create WeakAuras displays for each message type
+local function CreateWeakAurasDisplay(msgType, config)
+    local display = {
+        id = string.format("CLE_%s", msgType),
+        regionType = "icon",
+        trigger = {
+            type = "custom",
+            custom = string.format([[
+                function(trigger)
+                    if trigger.msgType == "%s" then
+                        return true
+                    end
+                    return false
+                end
+            ]], msgType),
+            events = {"CLE_MESSAGE"}
+        },
+        load = {
+            class = {multi = {}}
+        },
+        animation = {
+            start = {
+                type = "custom",
+                duration = 0.2,
+                alpha = 0,
+                scale = 0.1
+            },
+            main = {
+                type = "custom",
+                duration = config.displayDuration,
+                alpha = 1,
+                scale = config.scale
+            },
+            finish = {
+                type = "custom",
+                duration = 0.2,
+                alpha = 0,
+                scale = 0.1
+            }
+        },
+        icon = config.texture,
+        iconColor = config.color,
+        desaturate = false,
+        frameStrata = "HIGH",
+        width = config.size,
+        height = config.size,
+        xOffset = config.position.x,
+        yOffset = config.position.y,
+        anchorPoint = config.position.relativeTo,
+        anchorFrameType = "SCREEN",
+        selfPoint = "CENTER",
+        cooldown = false,
+        conditions = {},
+        config = {},
+        authorOptions = {},
+        information = {
+            forceEvents = true
+        }
+    }
+    
+    return display
 end
 
 -- Update the WeakAura module settings when a new message is caught
